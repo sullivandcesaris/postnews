11cc3171f250ae8a10adfd80619502a70c61fee2
sullivandcesaris <sullivandcesaris.dev@gmail.com>
2022-08-08 23:51:55 -0300

    APP: components and pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +11,19 @@ const Home: NextPage = () => {
       <Head>
         <title>Home | Post News</title>
       </Head>
-      <h1 className='title'>Hello World!</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br /> the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
