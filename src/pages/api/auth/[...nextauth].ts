a65af75192949354dd79454e86adbac1ae3da8af
sullivandcesaris <sullivandcesaris.dev@gmail.com>
2022-08-09 15:33:28 -0300

    SIGN IN: using faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
-    // Configure one or more authentication providers
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
@@ -10,7 +14,49 @@ export default NextAuth({
                 url: "https://github.com/login/oauth/authorize",
                 params: { scope: "read:user" },
             },
+
         }),
-        // ...add more providers here
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user
+
+            try {
+
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email!)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } },
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email!)
+                            )
+                        )
+                    )
+                )
+                .then((ret) => console.log(ret))
+                .catch((err) => console.error(
+                    'Error: [%s] %s: %s',
+                    err.name,
+                    err.message,
+                    err.errors()[0].description,
+                ))
+
+                return true;
+            } catch {
+                return false;
+            }
+        },
+    }
 })
