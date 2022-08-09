7be728b35e21219989cf246c1475ce4184d0bebe
sullivandcesaris <sullivandcesaris.dev@gmail.com>
2022-08-08 22:00:38 -0300

    APP: application structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | Post News</title>
+      </Head>
+      <h1 className='title'>Hello World!</h1>
+    </>
+  )
+}
+
+export default Home
